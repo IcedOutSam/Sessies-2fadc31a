7afaae4d6525dc7c00c919473f22689ccb7c9523
Sam <2102926@talnet.nl>
2020-04-25 01:12:10 +0200

    Uitwerking van de exercise

--- cart.php
+++ cart.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+session_start();
+?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -11,7 +14,28 @@
     <form method='post'>
     <label for='text'> Voeg een product toe aan uw winkelwagen: </label>
     <input type='text' name='name' id='0123'>
-    <input type='submit' value='Submit'>
+    <input type='submit' name="Laurens" value='Submit'>
     </form>
-  <?php
-$product = $_POST["name"];
+    <?php
+
+
+$Zeewater      = "Zeewater";
+$_SESSION["1"] = $Zeewater;
+
+$Zwembad       = "Zwembadwater";
+$_SESSION["2"] = $Zwembad;
+
+$Na            = "Natriumchloride";
+$_SESSION["3"] = $Na;
+
+if (isset($_POST["Laurens"])) {
+    if ($_SESSION[$_POST["name"]]) {
+        echo "Gekozen item is " . $_SESSION[$_POST["name"]];
+    } else {
+        echo "Geen valide product gevonden";
+    }
+}
+
+?>
+ </body>
+</html>
